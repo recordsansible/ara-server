5d8fd487a7c41fc86ac8d9e77d854b877f58c4a4
Florian Apolloner <florian@apolloner.eu>
2018-09-26 15:37:13 +0200

    Small cleanups for django best practices.
    Change-Id: I409bd3f7faa5f133fcb204613baebf2512d34c1a

--- ara/api/urls.py
+++ ara/api/urls.py
@@ -15,29 +15,29 @@
 #  You should have received a copy of the GNU General Public License
 #  along with ARA.  If not, see <http://www.gnu.org/licenses/>.
 
-from django.conf.urls import url
+from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from ara.api import views
 
 urlpatterns = [
-    url(r'^/$', views.api_root),
-    url(r'^/labels$', views.LabelList.as_view(), name='label-list'),
-    url(r'^/labels/(?P<pk>[0-9]+)$', views.LabelDetail.as_view(), name='label-detail'),
-    url(r'^/playbooks$', views.PlaybookList.as_view(), name='playbook-list'),
-    url(r'^/playbooks/(?P<pk>[0-9]+)$', views.PlaybookDetail.as_view(), name='playbook-detail'),
-    url(r'^/playbooks/(?P<pk>[0-9]+)/files$', views.PlaybookFilesDetail.as_view(), name='playbook-file-detail'),
-    url(r'^/plays$', views.PlayList.as_view(), name='play-list'),
-    url(r'^/plays/(?P<pk>[0-9]+)$', views.PlayDetail.as_view(), name='play-detail'),
-    url(r'^/tasks$', views.TaskList.as_view(), name='task-list'),
-    url(r'^/tasks/(?P<pk>[0-9]+)$', views.TaskDetail.as_view(), name='task-detail'),
-    url(r'^/hosts$', views.HostList.as_view(), name='host-list'),
-    url(r'^/hosts/(?P<pk>[0-9]+)$', views.HostDetail.as_view(), name='host-detail'),
-    url(r'^/results$', views.ResultList.as_view(), name='result-list'),
-    url(r'^/results/(?P<pk>[0-9]+)$', views.ResultDetail.as_view(), name='result-detail'),
-    url(r'^/files$', views.FileList.as_view(), name='file-list'),
-    url(r'^/files/(?P<pk>[0-9]+)$', views.FileDetail.as_view(), name='file-detail'),
-    url(r'^/stats$', views.StatsList.as_view(), name='stats-list'),
-    url(r'^/stats/(?P<pk>[0-9]+)$', views.StatsDetail.as_view(), name='stats-detail'),
+    path('', views.api_root),
+    path('labels', views.LabelList.as_view(), name='label-list'),
+    path('labels/<int:pk>', views.LabelDetail.as_view(), name='label-detail'),
+    path('playbooks', views.PlaybookList.as_view(), name='playbook-list'),
+    path('playbooks/<int:pk>', views.PlaybookDetail.as_view(), name='playbook-detail'),
+    path('playbooks/<int:pk>/files', views.PlaybookFilesDetail.as_view(), name='playbook-file-detail'),
+    path('plays', views.PlayList.as_view(), name='play-list'),
+    path('plays/<int:pk>', views.PlayDetail.as_view(), name='play-detail'),
+    path('tasks', views.TaskList.as_view(), name='task-list'),
+    path('tasks/<int:pk>', views.TaskDetail.as_view(), name='task-detail'),
+    path('hosts', views.HostList.as_view(), name='host-list'),
+    path('hosts/<int:pk>', views.HostDetail.as_view(), name='host-detail'),
+    path('results', views.ResultList.as_view(), name='result-list'),
+    path('results/<int:pk>', views.ResultDetail.as_view(), name='result-detail'),
+    path('files', views.FileList.as_view(), name='file-list'),
+    path('files/<int:pk>', views.FileDetail.as_view(), name='file-detail'),
+    path('stats', views.StatsList.as_view(), name='stats-list'),
+    path('stats/<int:pk>', views.StatsDetail.as_view(), name='stats-detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
