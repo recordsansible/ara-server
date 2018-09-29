0082277a2bdf0d0587fa9940155769f8702e3281
Florian Apolloner <florian@apolloner.eu>
2018-09-29 22:18:36 +0200

    Run black & isort over the codebase.
    Change-Id: I084279e15560a1178376d8ef1ed91ea84c060982

--- ara/server/urls.py
+++ ara/server/urls.py
@@ -1,8 +1,7 @@
-from django.urls import include, path
 from django.contrib import admin
-
+from django.urls import include, path
 
 urlpatterns = [
-    path('api/v1/', include('ara.api.urls')),
-    path('admin/', admin.site.urls),
+    path("api/v1/", include("ara.api.urls")),
+    path("admin/", admin.site.urls),
 ]
