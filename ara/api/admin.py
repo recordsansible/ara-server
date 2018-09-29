c4746cdd8543a26fe5e96892e0fa248593e53695
Florian Apolloner <florian@apolloner.eu>
2018-09-29 22:18:36 +0200

    Run black & isort over the codebase.
    Change-Id: I084279e15560a1178376d8ef1ed91ea84c060982

--- ara/api/admin.py
+++ ara/api/admin.py
@@ -1,12 +1,13 @@
-from ara.api import models
 from django.contrib import admin
 from django.contrib.auth.models import Group
 
+from ara.api import models
+
 
 class RecordAdmin(admin.ModelAdmin):
-    list_display = ('id', 'key', 'value', 'type')
-    search_fields = ('key', 'value', 'type')
-    ordering = ('key',)
+    list_display = ("id", "key", "value", "type")
+    search_fields = ("key", "value", "type")
+    ordering = ("key",)
 
 
 admin.site.register(models.Record, RecordAdmin)
