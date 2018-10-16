74ddc672494096547ba014cd054324d4bbf62220
Florian Apolloner <florian@apolloner.eu>
2018-10-16 22:23:23 +0200

    Split settings into multiple files (dev/prod/test).
    Change-Id: Ie829a8a8ce6cdc80d52be55ee68c920b4199dc98

--- ara/server/settings/base.py
+++ ara/server/settings/base.py
@@ -1,25 +1,10 @@
-import logging
 import os
 import sys
+from pathlib import Path
 
-from django.conf import global_settings
-from django.utils.crypto import get_random_string
 from envparse import env
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-
-def get_secret_key(secret_key):
-    if not secret_key:
-        return get_random_string(length=50)
-    return secret_key
-
-
-SECRET_KEY = env('SECRET_KEY', preprocessor=get_secret_key, default=None)
-
-DEBUG = env.bool('DJANGO_DEBUG', default=False)
-
-ALLOWED_HOSTS = env('ALLOWED_HOSTS', cast=list, default=['localhost', '127.0.0.1', 'testserver'])
+BASE_DIR = Path(os.path.abspath(__file__)).parents[3].as_posix()
 
 ADMINS = ()
 
@@ -47,14 +32,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
-
-# Django built-in server and npm development server
-CORS_ORIGIN_WHITELIST = (
-    '127.0.0.1:8000',
-    'localhost:3000',
-)
-
 ROOT_URLCONF = 'ara.server.urls'
 APPEND_SLASH = False
 
@@ -78,12 +55,8 @@ WSGI_APPLICATION = 'ara.server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': env('DATABASE_ENGINE', default='django.db.backends.sqlite3'),
-        'NAME': env('DATABASE_NAME', default=os.path.join(BASE_DIR, 'db.sqlite3')),
-        'USER': env('DATABASE_USER', default=None),
-        'PASSWORD': env('DATABASE_PASSWORD', default=None),
-        'HOST': env('DATABASE_HOST', default=None),
-        'PORT': env('DATABASE_PORT', default=None),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -158,36 +131,3 @@ REST_FRAMEWORK = {
     ),
     'TEST_REQUEST_DEFAULT_FORMAT': 'json'
 }
-
-
-class DisableMigrations(object):
-    def __contains__(self, item):
-        return True
-
-    def __getitem__(self, item):
-        return None
-
-
-TESTS_IN_PROGRESS = False
-if 'test' in sys.argv[1:] or 'jenkins' in sys.argv[1:]:
-    logging.disable(logging.CRITICAL)
-    PASSWORD_HASHERS = (
-        'django.contrib.auth.hashers.MD5PasswordHasher',
-    )
-    DEBUG = False
-    TEMPLATE_DEBUG = False
-    TESTS_IN_PROGRESS = True
-    MIGRATION_MODULES = DisableMigrations()
-
-if DEBUG:
-    EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.console.EmailBackend')
-else:
-    EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', global_settings.DEFAULT_FROM_EMAIL)
-EMAIL_HOST = os.getenv('EMAIL_HOST', 'localhost')
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
-EMAIL_PORT = env.int('EMAIL_PORT', default=25)
-EMAIL_SUBJECT_PREFIX = os.getenv('EMAIL_SUBJECT_PREFIX', '[Ara] ')
-EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS', default=False)
-EMAIL_USE_SSL = env.bool('EMAIL_USE_SSL', default=False)
---
+++ ara/server/settings/test.py
@@ -0,0 +1,32 @@
+import logging
+
+from .base import *  # noqa
+
+# TODO: Why is this here?
+logging.disable(logging.CRITICAL)
+
+DEBUG = False
+
+SECRET_KEY = 'TEST_SECRET_KEY'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+)
+
+
+class DisableMigrations(object):
+    def __contains__(self, item):
+        return True
+
+    def __getitem__(self, item):
+        return None
+
+
+MIGRATION_MODULES = DisableMigrations()
