e8d9134868c2a0eb5ee20a4cf91407db94b976a9
David Moreau Simard <dmsimard@redhat.com>
2018-12-13 14:47:31 -0500

    Refactor configuration to use dynaconf
    This changes the configuration engine from everett to dynaconf.
    dynaconf allows loading configuration from files (json, ini, yaml, toml)
    as well as environment variables prefixed by ARA_.
    
    Our usage of dynaconf is similar to the use case from the Pulp [1]
    project and they have documented an issue when loading database
    parameters [2]. This issue is worked around by importing dynaconf in the
    different entry points.
    
    This introduces some other changes as well:
    - We're now creating a default configuration and data directory at
      ~/.ara. The location of this directory is controlled with the
      ARA_BASE_DIR environment variable.
    - We're now creating a default configuration template in
      ~/.ara/default_config.yaml.
    - The default database is now located at ~/.ara/ara.sqlite. The location
      of this database can be customized with the ARA_DATABASE_NAME
      environment variable.
      Note that ARA 0.x used "~/.ara/ansible.sqlite" -- the file name change
      is deliberate in order to avoid user databases clashing between
      versions.
    
    More documentation on this will be available in an upcoming patch.
    
    [1]: https://github.com/pulp/pulp
    [2]: https://github.com/rochacbruno/dynaconf/issues/89
    
    Change-Id: I8178b4ca9f2b4d7f4c45c296c08391e84e8b990d

--- ara/server/wsgi.py
+++ ara/server/wsgi.py
@@ -2,6 +2,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ara.server.settings")
+# https://github.com/rochacbruno/dynaconf/issues/89
+from dynaconf.contrib import django_dynaconf  # noqa
 
 application = get_wsgi_application()
