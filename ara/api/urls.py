e18ad1de3553c16bc33cae4f64eabb0112cfa6a6
David Moreau Simard <dmsimard@redhat.com>
2018-12-18 12:04:10 -0500

    Refactor and standardize how files are added to playbooks
    Files are now (once again) related to playbooks.
    We expect the playbook to be created first and then it's files are
    created and associated to it.
    
    - the /api/v1/playbook/<id>/files endpoint was removed
    - the playbook id is now required when doing a POST on /api/v1/files
    - playbook.file referenced a file object and no longer exists.
    - playbook.file was replaced by "path" which is the path for the playbook
      file.
    - playbook.files still exists but, from an API standpoint, this means
      that to find the files associated to a playbook, we can now do
      something like:
    
        "/api/v1/files?playbook=%s" % playbook.id
    
    To find the playbook file itself, we can do something like:
    
        "/api/v1/files?playbook=%s&path=%s" % (playbook.id, playbook.path)
    
    Change-Id: Id51129757e1626313caee4005b081027e5694aba

--- ara/api/urls.py
+++ ara/api/urls.py
@@ -21,6 +21,7 @@ from ara.api import views
 
 router = ExtendedDefaultRouter(trailing_slash=False)
 router.register("labels", views.LabelViewSet, base_name="label")
+router.register("playbooks", views.PlaybookViewSet, base_name="playbook")
 router.register("plays", views.PlayViewSet, base_name="play")
 router.register("tasks", views.TaskViewSet, base_name="task")
 router.register("hosts", views.HostViewSet, base_name="host")
@@ -28,8 +29,5 @@ router.register("results", views.ResultViewSet, base_name="result")
 router.register("files", views.FileViewSet, base_name="file")
 router.register("records", views.RecordViewSet, base_name="record")
 router.register("stats", views.StatsViewSet, base_name="stats")
-
-playbook_routes = router.register("playbooks", views.PlaybookViewSet, base_name="playbook")
-playbook_routes.register("files", views.PlaybookFilesDetail, base_name="file", parents_query_lookups=["playbooks"])
 
 urlpatterns = router.urls
