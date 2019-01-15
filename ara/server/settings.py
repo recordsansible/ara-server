f4ab78e514473ae9b5452a2c7a617d8927a010cb
David Moreau Simard <dmsimard@redhat.com>
2019-01-15 10:44:26 -0500

    Create a persistent default random secret key if none are set
    If there are no SECRET_KEY or ARA_SECRET_KEY environment variables
    set or if there is no configuration file with the SECRET_KEY variable
    set, settings.py will generate a random secret key once and it will
    be persisted to the default configuration.
    
    Change-Id: Idf2f1ee5f8a70bc2411de51a154bc4504705d89a

--- ara/server/settings.py
+++ ara/server/settings.py
@@ -1,4 +1,5 @@
 import os
+import random
 import textwrap
 
 import yaml
@@ -19,7 +20,12 @@ CORS_ORIGIN_ALLOW_ALL = settings.get("CORS_ORIGIN_ALLOW_ALL", False)
 
 ADMINS = settings.get("ADMINS", ())
 
-SECRET_KEY = settings.get("SECRET_KEY")
+def get_secret_key():
+    if not settings.get("SECRET_KEY"):
+        return "".join([random.choice("abcdefghijklmnopqrstuvwxyz0123456789@#$^()") for i in range(50)])
+    return settings.get("SECRET_KEY")
+
+SECRET_KEY = get_secret_key()
 
 # We're not expecting ARA to use multiple concurrent databases.
 # Make it easier for users to specify the configuration for a single database.
@@ -157,7 +163,7 @@ if not os.path.exists(DEFAULT_CONFIG):
         ALLOWED_HOSTS=ALLOWED_HOSTS,
         CORS_ORIGIN_WHITELIST=CORS_ORIGIN_WHITELIST,
         CORS_ORIGIN_ALLOW_ALL=CORS_ORIGIN_ALLOW_ALL,
-        SECRET_KEY="please-change-this",
+        SECRET_KEY=SECRET_KEY,
         DATABASES=DATABASES,
         STATIC_URL=STATIC_URL,
         STATIC_ROOT=STATIC_ROOT,
@@ -178,3 +184,5 @@ if not os.path.exists(DEFAULT_CONFIG):
         """
         config_file.write(textwrap.dedent(comment))
         yaml.dump({"default": CONFIG}, config_file, default_flow_style=False)
+
+ARA_SETTINGS = os.getenv("ARA_SETTINGS", DEFAULT_CONFIG)
